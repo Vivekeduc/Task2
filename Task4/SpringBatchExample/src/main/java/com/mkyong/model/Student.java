ecff8fb7b494e8cab093ee043930a6a5b0c6fe11
Vivekeduc <vivek.educ@gmail.com>
2017-07-21 15:07:26 -0500

    Task4

---
+++ Task4/SpringBatchExample/src/main/java/com/mkyong/model/Student.java
@@ -0,0 +1,42 @@
+package com.mkyong.model;
+
+public class Student {
+
+	private String StudentId;
+	private String FirstName;
+	private String LastName;
+	private String Email;
+
+	public String getStudentId() {
+		return StudentId;
+	}
+
+	public void setStudentId(String studentId) {
+		StudentId = studentId;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+}
